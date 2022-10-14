3f69eef57d9e5176c7adcd489513df1867408cd7
Caephas <arinzeobidiegwu@gmail.com>
2022-10-14 16:26:53 +0100

    Router separation of logic

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,23 +1,22 @@
-import { Router, Request, Response, NextFunction } from "express"
-import { get } from './decorators/routes'
+import { Request, Response } from 'express';
+import { get, controller } from './decorators';
 
-
-@controller('/')
-class LoginController{
+@controller('/auth')
+class LoginController {
     @get('/login')
-    getLogin(req: Request, res: Response) {
-    res.send(`
-        <form method="POST">
-            <div>
-                <label>Email</label>
-                <input type="email" name="email"/>
-            </div>
-            <div>
-                <label>Password</label>
-                <input type="password" name="password"/>
-            </div>
-            <button>Submit</button>
-        </form>
-    `)
-}
+    getLogin(req: Request, res: Response): void {
+        res.send(`
+      <form method="POST">
+        <div>
+          <label>Email</label>
+          <input name="email" />
+        </div>
+        <div>
+          <label>Password</label>
+          <input name="password" type="password" />
+        </div>
+        <button>Submit</button>
+      </form>
+    `);
+    }
 }
